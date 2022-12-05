ecca9f2280e0545b0f6fb2560ed4a279797bca61
Hienttk65 <117137504+Hienttk65@users.noreply.github.com>
2022-12-05 23:52:21 +0700

    Update TranThuyHien_Bai1.cpp

--- TranThuyHien_Bai1.cpp
+++ TranThuyHien_Bai1.cpp
@@ -1,54 +1,134 @@
-#include <iostream>
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h> 
-
+#include<iostream>
+#include<string>
 using namespace std;
-struct Node {
-    int num;
-    struct Node *Next;
+struct Ngay { 
+	int ngay;
+	int thang;
+	int nam; 
 };
-
-typedef struct Node *PToNode;
-
+struct SinhVien { 
+	int maSV; 
+	string hoTen;
+	char gioiTinh[20];
+	Ngay ngaySinh;
+	string diaChi;
+	char lop[12];
+	char khoa[7]; 
+};
+struct Node {
+	SinhVien data;
+	Node* link;
+};
+struct List { 
+	Node *first;
+	Node *last;
+};
+Node* CreateNode(SinhVien x)
+{
+	Node* p;
+	p = new Node;
+	if (p == NULL)  exit(1);
+	p->data = x;
+	p->link = NULL;
+	return p;
+}
+void Nhap(SinhVien *a,int n) {
+	a = new SinhVien[100];
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << "Nhap ma sv: ";
+		cin >> a[i].maSV;
+		cin.ignore();
+		cout << "Nhap ho ten: ";
+		getline(cin, a[i].hoTen);
+		cout << "Nhap lop: ";
+		cin >> a[i].lop;
+		cout << "Nhap khoa: ";
+		cin >> a[i].khoa;
+		cout << "Nhap ngay sinh:";
+		cin >> a[i].ngaySinh.ngay >> a[i].ngaySinh.thang >> a[i].ngaySinh.nam;
+		cout << "Nhap gioi tinh: ";
+		cin >> a[i].gioiTinh;
+		cout << "Nhap dia chi: ";
+		cin.ignore();
+		getline(cin, a[i].diaChi);
+	}
+}
+void ThemSV(SinhVien* a, int n) {
+	n++;
+	for (int  i =n-1; i >0 ; i--)
+	{
+		cout << "Nhap ma sv: ";
+		cin >> a[i].maSV;
+		cin.ignore();
+		cout << "Nhap ho ten: ";
+		getline(cin, a[i].hoTen);
+		cout << "Nhap lop: ";
+		cin >> a[i].lop;
+		cout << "Nhap khoa: ";
+		cin >> a[i].khoa;
+		cout << "Nhap ngay sinh:";
+		cin >> a[i].ngaySinh.ngay >> a[i].ngaySinh.thang >> a[i].ngaySinh.nam;
+		cout << "Nhap gioi tinh: ";
+		cin >> a[i].gioiTinh;
+		cout << "Nhap dia chi: ";
+		cin.ignore();
+		getline(cin, a[i].diaChi);
+	}
+}
+void XuatSV(SinhVien* a, int n) {
+	a = new SinhVien[200];
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << "Ma sv: " << a[i].maSV << endl;
+		cout << "Ho ten: "<<a[i].hoTen<<endl;
+		cout << "Lop: "<< a[i].lop<<endl;
+		cout << "Khoa: "<< a[i].khoa<<endl;
+		cout << "Ngay sinh:"<< a[i].ngaySinh.ngay <<"/"<<a[i].ngaySinh.thang<<"/"<<  a[i].ngaySinh.nam<<endl;
+		cout << "Gioi tinh: "<< a[i].gioiTinh<<endl;
+		cout << "Nhap dia chi: "<< a[i].diaChi<<endl;
+	}
+}
+void sapxep(SinhVien *a, int n) {
+	a = new SinhVien[100];
+	SinhVien tmp;
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			if (a[i].maSV > a[j].maSV) {
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+	}
+}
+void Timkiem(SinhVien* a, int n) {
+	a = new SinhVien[100];
+	int t = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			if ((a[i].ngaySinh.ngay == a[j].ngaySinh.ngay)&&(a[i].ngaySinh.thang == a[j].ngaySinh.thang)
+				&&(a[i].ngaySinh.nam == a[j].ngaySinh.nam)) {
+				cout << "Ma sv: " << a[i].maSV << endl;
+				cout << "Ho ten: " << a[i].hoTen << endl;
+				cout << "Lop: " << a[i].lop << endl;
+				cout << "Khoa: " << a[i].khoa << endl;
+				cout << "Ngay sinh:" << a[i].ngaySinh.ngay << "/" << a[i].ngaySinh.thang << "/" << a[i].ngaySinh.nam << endl;
+				cout << "Gioi tinh: " << a[i].gioiTinh << endl;
+				cout << "Nhap dia chi: " << a[i].diaChi << endl;
+				t++;
+			}
+		}
+	}
+	if (t == 0) cout << "Khong co sinh vien cung ngay";
+}
 int main() {
-    clock_t start, end;   
-    double time_use;
-	start = clock();
-    int n, m, i;
-    PToNode head, p, q;
-
-    cout<< "Please input n players: ";
-    cin>>n;
-    cout<< "Please input the mth player: ";
-    cin>>m;
-
-    head = (PToNode)malloc(sizeof(struct Node));
-    head->num = 1;
-
-    p = head;
-    for (i = 2; i <= n; i++) {
-        q = (PToNode)malloc(sizeof(struct Node));
-        q->num = i;
-        p->Next = q;
-        p = q;
-    }
-    p->Next = head;
-
-    while (q->Next != q) {
-        for (i = 1; i < m+1; i++)
-            q = q->Next;
-        p = q->Next;
-        cout<< p->num<<" ";
-        q->Next = p->Next;
-        free(p);
-    }
-    cout<<q->num<<" ";
-    free(q);
-    
-
-    end = clock(); 
-    time_use = (double)(end - start) / CLOCKS_PER_SEC;
-	cout<<"Fasting time is: " << time_use;
-    return 0;
+	int n; cin >> n;
+	SinhVien *hien=new SinhVien[200];
+	Nhap(hien, n);
+	sapxep(hien, n);
+	XuatSV(hien, n);
+	ThemSV(hien, n);
+	XuatSV(hien, n);
+	return 0;
 }

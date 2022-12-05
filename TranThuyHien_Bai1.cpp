6ab0df7fbe1ff92cd650ea69d1a5bc4c87783931
Hienttk65 <117137504+Hienttk65@users.noreply.github.com>
2022-12-05 22:12:37 +0700

    Add files via upload

---
+++ TranThuyHien_Bai1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> 
+
+using namespace std;
+struct Node {
+    int num;
+    struct Node *Next;
+};
+
+typedef struct Node *PToNode;
+
+int main() {
+    clock_t start, end;   
+    double time_use;
+	start = clock();
+    int n, m, i;
+    PToNode head, p, q;
+
+    cout<< "Please input n players: ";
+    cin>>n;
+    cout<< "Please input the mth player: ";
+    cin>>m;
+
+    head = (PToNode)malloc(sizeof(struct Node));
+    head->num = 1;
+
+    p = head;
+    for (i = 2; i <= n; i++) {
+        q = (PToNode)malloc(sizeof(struct Node));
+        q->num = i;
+        p->Next = q;
+        p = q;
+    }
+    p->Next = head;
+
+    while (q->Next != q) {
+        for (i = 1; i < m+1; i++)
+            q = q->Next;
+        p = q->Next;
+        cout<< p->num<<" ";
+        q->Next = p->Next;
+        free(p);
+    }
+    cout<<q->num<<" ";
+    free(q);
+    
+
+    end = clock(); 
+    time_use = (double)(end - start) / CLOCKS_PER_SEC;
+	cout<<"Fasting time is: " << time_use;
+    return 0;
+}
